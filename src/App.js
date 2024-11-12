edd83ef52b1193129fbeb385ff3eff63e028de32
xHelpy <xiozeerpl@gmail.com>
2024-11-12 13:57:30 +0100

    add files

--- src/App.js
+++ src/App.js
@@ -2,23 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let firstdate = Date.now()
+  let seconddate =  new Date("1900-03-23")
+
+  let diff = firstdate - seconddate.getTime()
+
+  let Days = diff / 86400000
+  let Months = diff / 2629800000
+    let Years = diff / 31536000000
+  console.log(firstdate,diff,Days)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+
+
+          <label>Lata roznicy: {Math.trunc(Years)} </label>
+          <label>Miesiace roznicy: {Math.trunc(Months%12)} </label>
+          <label>Dni: {Math.trunc((Days%365)%30)}</label>
+      </div>
   );
 }
 
